545918538cf6966c095954fc0645b383c6fb9d40
Jason Devaraj <jdevaraj01@gmail.com>
2024-03-27 18:36:30 -0400

    Change to typescript

---
+++ cw.js
@@ -0,0 +1,31 @@
+/* Function. Main
+*/
+function main() {
+    return "hi";
+}
+/* Function wc_message
+*/
+/* Function byte_count
+*/
+/* Function word count
+*/
+/*Function char_count
+*/
+/* Function get_all
+*/
+/* Function main method
+*/
+// stringLength.ts
+// Retrieve the string from the command line arguments
+var inputString = process.argv[2];
+// Check if the input string is provided
+if (!inputString) {
+    console.log("Please provide a string as an argument.");
+}
+else {
+    // Calculate the length of the string
+    var length_1 = inputString.length;
+    // Output the length of the string
+    console.log(main());
+}
+//ts-node typescript-file.ts
---
+++ cw.ts
@@ -0,0 +1,31 @@
+/* Function. Main
+*/
+function main(){
+    return "hi";
+} 
+
+/* Function wc_message
+*/
+
+/* Function byte_count
+*/
+
+/* Function word count
+*/
+
+/*Function char_count
+*/
+
+/* Function get_all
+*/
+
+/* Function main method 
+*/
+
+
+
+const inputString: string = process.argv[2];
+
+// Send argument to main function 
+
+
